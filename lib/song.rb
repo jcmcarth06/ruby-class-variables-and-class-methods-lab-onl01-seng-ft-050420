30bda8399f08ba97587b0f683c5f2a89eef13725
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-14 03:25:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,12 @@
 class Song
 
   attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@artists = []
   @@genres = []
 
-  def new (name, artist, genre)
+  def initialize (name, artist, genre)
     
 
   end
